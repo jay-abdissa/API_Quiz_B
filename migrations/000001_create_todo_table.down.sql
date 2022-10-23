4a566d19d48eb82c99bc333ff98f1e64f3e862e8
jadah <2018118161@ub.edu.bz>
2022-10-23 09:16:13 -0700

    migration for items table

---
+++ migrations/000001_create_todo_table.down.sql
@@ -0,0 +1,3 @@
+-- Filename: migrations/000001_create_todo_table_down.sql
+
+DROP TABLE IF EXISTS items;
---
+++ migrations/000001_create_todo_table.up.sql
@@ -0,0 +1,8 @@
+-- Filename: migrations/000001_create_todo_table_up.sql
+
+CREATE TABLE IF NOT EXISTS items (
+    id bigserial PRIMARY KEY,
+    name text NOT NULL,
+    description text NOT NULL,
+    status text NOT NULL,
+);
