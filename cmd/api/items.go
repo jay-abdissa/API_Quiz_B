8a4b31f6e947c5f31324c4ae5c50544b94f639ef
jadah <2018118161@ub.edu.bz>
2022-10-23 13:19:45 -0700

    rest of crud functionality + partial updates

--- cmd/api/items.go
+++ cmd/api/items.go
@@ -3,6 +3,7 @@ package main
 
 import	(
 	"fmt"
+	"errors"
 	"net/http"
 	"quiz3.castillojadah.net/internals/data"
 	"quiz3.castillojadah.net/internals/validator"
@@ -22,7 +23,7 @@ func (app *application) createToDoHandler(w http.ResponseWriter, r *http.Request
 		app.badRequestResponse(w, r, err)
 		return
 	}
-	entries := &data.Items{
+	items:= &data.Items{
 		Name: input.Name,
 		Description: input.Description,
 		Status: input.Status,
@@ -30,7 +31,7 @@ func (app *application) createToDoHandler(w http.ResponseWriter, r *http.Request
 	//initialize a new validator instance
 	v := validator.New()
 	//check the map to determine if there were any validation errors
-	if data.ValidateEntries(v,entries); !v.Valid(){
+	if data.ValidateItems(v,items); !v.Valid(){
 		app.failedValidationResponse(w,r,v.Errors)
 		return
 	}
@@ -48,4 +49,127 @@ func (app *application) createToDoHandler(w http.ResponseWriter, r *http.Request
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
 	}
+}
+
+func (app *application) showToDoHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(r)
+	if err != nil {
+		app.notFoundResponse(w, r)
+		return
+	}
+
+	// Fetch the specific items
+	items, err := app.models.Items.Get(id)
+	// Handle errors
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+	// Write the data returned by Get()
+	err = app.writeJSON(w, http.StatusOK, envelope{"items": items}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
+func (app *application) updateToDoHandler(w http.ResponseWriter, r *http.Request) {
+	// This method does a partial replacement
+	// Get the id for the school that needs updating
+	id, err := app.readIDParam(r)
+	if err != nil {
+		app.notFoundResponse(w, r)
+		return
+	}
+	// Fetch the orginal record from the database
+	items, err := app.models.Items.Get(id)
+	// Handle errors
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+	// Create an input struct to hold data read in from the client
+	// We update input struct to use pointers because pointers have a
+	// default value of nil
+	// If a field remains nil then we know that the client did not update it
+	var input struct {
+		Name    *string  `json:"name"`
+		Description  *string  `json:"description"`
+		Status *string  `json:"status"`
+	}
+
+	// Initialize a new json.Decoder instance
+	err = app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+	// Check for updates
+	if input.Name != nil {
+		items.Name = *input.Name
+	}
+	if input.Description != nil {
+		items.Description= *input.Description
+	}
+	if input.Status!= nil {
+		items.Status = *input.Status
+	}
+
+	// Perform validation on the updated items. If validation fails, then
+	// we send a 422 - Unprocessable Entity respose to the client
+	// Initialize a new Validator instance
+	v := validator.New()
+
+	// Check the map to determine if there were any validation errors
+	if data.ValidateItems(v, items); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+	// Pass the updated School record to the Update() method
+	err = app.models.Items.Update(items)
+	if err != nil {
+			app.serverErrorResponse(w, r, err)
+			return
+		}
+	// Write the data returned by Get()
+	err = app.writeJSON(w, http.StatusOK, envelope{"items": items}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
+
+func (app *application) deleteToDoHandler(w http.ResponseWriter, r *http.Request) {
+	// Get the id for the itesm that needs updating
+	id, err := app.readIDParam(r)
+	if err != nil {
+		app.notFoundResponse(w, r)
+		return
+	}
+	// Delete the items from the database. Send a 404 Not Found status code to the
+	// client if there is no matching record
+	err = app.models.Items.Delete(id)
+	// Handle errors
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+	// Return 200 Status OK to the client with a success message
+	err = app.writeJSON(w, http.StatusOK, envelope{"message": "items successfully deleted"}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+
 }
--- internals/data/data.go
+++ internals/data/data.go
@@ -3,7 +3,11 @@
 package data
 
 import (
+	"context"
 	"database/sql"
+	"errors"
+	//"fmt"
+	"time"
 
 	"quiz3.castillojadah.net/internals/validator"
 )
@@ -15,7 +19,7 @@ type Items struct {
 	Status string `json:"status"`
 }
 
-func ValidateEntries(v *validator.Validator, entries *Items)  {
+func ValidateItems(v *validator.Validator, entries *Items)  {
 	//use the check method to execute our validation checks
 	v.Check(entries.Name != "", "name", "must be provided")
 	v.Check(len(entries.Name) <= 200, "name", "must not be more than 200 bytes long")
@@ -34,7 +38,7 @@ type ItemModel struct {
 //Insert allows us to create another to do list
 func (m ItemModel) Insert(items *Items) error {
 	query := `
-		INSERT INTO items (name, decription, status)
+		INSERT INTO items (name, description, status)
 		VALUES ($1, $2, $3)
 		RETURNING id
 	`
@@ -43,20 +47,102 @@ func (m ItemModel) Insert(items *Items) error {
 		items.Name, items.Description,
 		items.Status,
 	}
-	return m.DB.QueryRowContext(query, args...).Scan(&items.ID)
+	// Create a context
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	// Cleanup to prevent memory leaks
+	defer cancel()
+	return m.DB.QueryRowContext(ctx, query, args...).Scan(&items.ID)
 }
 
 //Insert allows us to get another to do list
 func (m ItemModel) Get(id int64) (*Items, error) {
-	return nil, nil
+	// Ensure that there is a valid id
+	if id < 1 {
+		return nil, ErrRecordNotFound
+	}
+	// Create the query
+	query := `
+		SELECT id, name, description, status
+		FROM items
+		WHERE id = $1
+	`
+	// Declare a items variable to hold the returned data
+	var items Items
+	// Create a context
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	// Cleanup to prevent memory leaks
+	defer cancel()
+	// Execute the query using QueryRow()
+	err := m.DB.QueryRowContext(ctx, query, id).Scan(
+		&items.ID,
+		&items.Name,
+		&items.Description,
+		&items.Status,
+	)
+	// Handle any errors
+	if err != nil {
+		// Check the type of error
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return nil, ErrRecordNotFound
+		default:
+			return nil, err
+		}
+	}
+	// Success
+	return &items, nil
 }
 
 //Insert allows us to update another to do list
 func (m ItemModel) Update(items *Items) error {
-	return nil
+	// Create a query
+	query := `
+		UPDATE items
+		SET name = $1, description = $2, status = $3,
+		WHERE id = $4
+	`
+	args := []interface{}{
+		items.Name,
+		items.Description,
+		items.Status,
+		items.ID,
+	}
+	// Create a context
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	// Cleanup to prevent memory leaks
+	defer cancel()
+	return m.DB.QueryRowContext(ctx, query, args...).Scan(&items.ID)
 }
 
 //Insert allows us to delete another to do list
 func (m ItemModel) Delete(id int64) error {
+	// Ensure that there is a valid id
+	if id < 1 {
+		return ErrRecordNotFound
+	}
+	// Create the delete query
+	query := `
+		DELETE FROM items
+		WHERE id = $1
+	`
+	// Create a context
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	// Cleanup to prevent memory leaks
+	defer cancel()
+	// Execute the query
+	result, err := m.DB.ExecContext(ctx, query, id)
+	if err != nil {
+		return err
+	}
+	// Check how many rows were affected by the delete operation. We
+	// call the RowsAffected() method on the result variable
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+	// Check if no rows were affected
+	if rowsAffected == 0 {
+		return ErrRecordNotFound
+	}
 	return nil
 }
